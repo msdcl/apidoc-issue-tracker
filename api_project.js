41799eca59b2ff23828ba34660122bb1978e4108
Mahendra-Singh-Chauhan <m.chauhankkm@gmail.com>
2018-10-07 19:34:10 +0530

    apidoc

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Issue tracker",
+  "version": "0.0.1",
+  "description": "Api documentation for Issue tracker",
+  "title": "Issue tracker APIs",
+  "url": "http://localhost:3004",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-07T14:00:12.996Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
